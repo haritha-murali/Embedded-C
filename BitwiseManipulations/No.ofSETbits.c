f8c2c19f0f55c5697a0d3d494bdd19a32e1abfc0
haritha-murali <88222222+haritha-murali@users.noreply.github.com>
2025-05-26 15:58:57 +0530

    Create No.ofSETbits.c

---
+++ BitwiseManipulations/No.ofSETbits.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void noSETbitsArray(){            //no. of set bits in a array
+  int a[]={1,4};
+  int c=0;
+  int n=sizeof(a)/sizeof(a[0]);
+  for(int i=0;i<n;i++){
+      int x=a[i];
+      while(x!=0){
+          if((x&1)!=0){c++;}
+          x>>=1;
+      }
+  }
+  printf("%d\n",c);
+}
+
+
+void noSETbitsNumber(){            //no. of set bits in a number
+  int x=15;
+  int t=x;
+  int c=0;
+  int n=0;
+  while(x!=0){
+      n++;
+      if((x&1)!=0){c++;}
+      x>>=1;
+      }  
+  printf("set bits in %d is %d\n",t,c);
+}
+
+int main()
+{
+    noSETbitsArray();
+    noSETbitsNumber();
+    return 0;
+}
