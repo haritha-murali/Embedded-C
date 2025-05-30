c2ea33d35979d7c3544d95f8346f0ccc767073db
haritha-murali <88222222+haritha-murali@users.noreply.github.com>
2025-05-30 17:03:55 +0530

    Count numbers in the range [L, R] having only three set bits

--- BitwiseManipulations/CountNumbers[L, R]_only3setbits.c
+++ BitwiseManipulations/CountNumbers[L, R]_only3setbits.c
@@ -13,3 +13,34 @@ Query(6, 12): Numbers in the range [6, 12] having three set bits are {7, 11}.
 */
 
 
+#include <stdio.h>
+
+int set_bits(int n1,int n2) {
+	int b=0,c=0,t=0;
+	for(int i=n1; i<=n2; i++) {
+		b=0;
+		t=i;
+		while(t) {
+			b+=(t&1);
+			t>>=1;
+		}
+		if(b==3) {
+			c++;
+		}
+	}
+	return c;
+}
+
+int main()
+{
+	int a[][2]= {{1, 10}, {6, 12}};
+	int n=sizeof(a)/sizeof(a[0][0]);
+	int sb=0;
+
+	for(int i=0; i<(n/2); i++) {
+		sb=set_bits(a[i][0],a[i][1]);
+		printf("%d ",sb);
+	}
+
+	return 0;
+}
