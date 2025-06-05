08162d05936be0ee68dfb5fee17be0bf71371df3
haritha-murali <88222222+haritha-murali@users.noreply.github.com>
2025-06-05 17:33:23 +0530

    Subsets pf an array & maximum XOR value generated from subsets of an array

--- BitwiseManipulations/subsetsOfSet.c
+++ BitwiseManipulations/subsetsOfSet.c
@@ -16,3 +16,33 @@ int main()
 	}
 	return 0;
 }
+
+
+//maximum XOR value generated from subsets of an array
+
+
+#include <stdio.h>
+
+
+int main()
+{
+    int a[]={1,2,3,4};
+    int n=sizeof(a)/sizeof(a[0]);
+    int subsets=1<<n;
+    int xr[20]={0};
+    int max=0;
+	for(int i=0;i<subsets;i++){
+	    for(int j=0;j<n;j++){
+	        if(i&(1<<j)){
+	            xr[i]^=a[j];
+	            if(xr[i]>max){
+	            max=xr[i];
+	            }
+	        }
+	    }
+	    printf("%d ",xr[i]); 
+	}
+	printf("\nmaximum XOR of subsets is %d\n",max);
+    
+    return 0;
+}
